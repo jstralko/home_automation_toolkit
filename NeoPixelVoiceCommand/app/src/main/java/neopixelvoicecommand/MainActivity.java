5d45619b20c554491813399e21fe855df6a13194
Gerald J Stralko <gerald.stralko@wolterskluwer.com>
2017-01-16 13:42:58 -0500

    add Bluetooth scanning.  Only printing device discovery in the logs

--- NeoPixelVoiceCommand/app/src/main/java/neopixelvoicecommand/MainActivity.java
+++ NeoPixelVoiceCommand/app/src/main/java/neopixelvoicecommand/MainActivity.java
@@ -1,8 +1,21 @@
 package neopixelvoicecommand;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattDescriptor;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,10 +26,20 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import neopixelvoicecommand.R;
+import java.util.UUID;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+import neopixelvoicecommand.R;
+import neopixelvoicecommand.ble.BleDevicesScanner;
+import neopixelvoicecommand.ble.BleManager;
+import neopixelvoicecommand.ble.BleUtils;
+
+public class MainActivity extends AppCompatActivity implements BleManager.BleManagerListener, BleUtils.ResetBluetoothAdapterListener, NavigationView.OnNavigationItemSelectedListener {
+
+    private static final String TAG = "NeopixelVoiceCommand";
+    private BleManager mBleManager;
+    private boolean mIsScanPaused = true;
+    private BleDevicesScanner mScanner;
+    private static final int PERMISSION_REQUEST_FINE_LOCATION = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +65,95 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        mBleManager = BleManager.getInstance(this);
+        // Request Bluetooth scanning persmissions
+        requestLocationPermissionIfNeeded();
+    }
+
+    // region Permissions
+    @TargetApi(Build.VERSION_CODES.M)
+    private void requestLocationPermissionIfNeeded() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // Android M Permission check 
+            if (this.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("This app needs location access");
+                builder.setMessage("Please grant location access so this app can scan for Bluetooth peripherals");
+                builder.setPositiveButton(android.R.string.ok, null);
+                builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    public void onDismiss(DialogInterface dialog) {
+                        requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_REQUEST_FINE_LOCATION);
+                    }
+                });
+                builder.show();
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_FINE_LOCATION: {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d(TAG, "Location permission granted");
+                    // Autostart scan
+                    autostartScan();
+                } else {
+                    final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("Bluetooth Scanning not available");
+                    builder.setMessage("Since location access has not been granted, the app will not be able to scan for Bluetooth peripherals");
+                    builder.setPositiveButton(android.R.string.ok, null);
+                    builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+
+                        @Override
+                        public void onDismiss(DialogInterface dialog) {
+                        }
+
+                    });
+                    builder.show();
+                }
+                break;
+            }
+            default:
+                break;
+        }
+    }
+
+    // endregion
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // Set listener
+        mBleManager.setBleListener(this);
+
+        // Autostart scan
+        autostartScan();
+    }
+
+    @Override
+    public void onPause() {
+        // Stop scanning
+        if (mScanner != null && mScanner.isScanning()) {
+            mIsScanPaused = true;
+            stopScanning();
+        }
+
+        super.onPause();
+    }
+
+    public void onStop() {
+        super.onStop();
+    }
+
+    private void autostartScan() {
+        if (BleUtils.getBleStatus(this) == BleUtils.STATUS_BLE_ENABLED) {
+            // If was connected, disconnect
+            mBleManager.disconnect();
+            startScan(null);
+        }
     }
 
     @Override
@@ -100,4 +212,85 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    // region BleManagerListener
+    @Override
+    public void onConnected() {
+    }
+
+    @Override
+    public void onConnecting() {
+    }
+
+    @Override
+    public void onDisconnected() {
+        Log.d(TAG, "MainActivity onDisconnected");
+    }
+
+    @Override
+    public void onServicesDiscovered() {
+        Log.d(TAG, "services discovered");
+    }
+
+    @Override
+    public void onDataAvailable(BluetoothGattCharacteristic characteristic) {
+    }
+
+    @Override
+    public void onDataAvailable(BluetoothGattDescriptor descriptor) {
+    }
+
+    @Override
+    public void onReadRemoteRssi(int rssi) {
+
+    }
+
+    // region ResetBluetoothAdapterListener
+    @Override
+    public void resetBluetoothCompleted() {
+        Log.d(TAG, "Reset completed -> Resume scanning");
+        resumeScanning();
+    }
+    // endregion
+
+    private void resumeScanning() {
+        if (mIsScanPaused) {
+            startScan(null);
+            mIsScanPaused = mScanner == null;
+        }
+    }
+
+    // region Scan
+    private void startScan(final UUID[] servicesToScan) {
+        Log.d(TAG, "startScan");
+
+        // Stop current scanning (if needed)
+        stopScanning();
+
+        // Configure scanning
+        BluetoothAdapter bluetoothAdapter = BleUtils.getBluetoothAdapter(getApplicationContext());
+        if (BleUtils.getBleStatus(this) != BleUtils.STATUS_BLE_ENABLED) {
+            Log.w(TAG, "startScan: BluetoothAdapter not initialized or unspecified address.");
+        } else {
+            mScanner = new BleDevicesScanner(bluetoothAdapter, servicesToScan, new BluetoothAdapter.LeScanCallback() {
+                @Override
+                public void onLeScan(final BluetoothDevice device, final int rssi, byte[] scanRecord) {
+                    final String deviceName = device.getName();
+                    Log.d(TAG, "Discovered device: " + (deviceName != null ? deviceName : "<unknown>"));
+                }
+            });
+
+            // Start scanning
+            mScanner.start();
+        }
+    }
+
+    private void stopScanning() {
+        // Stop scanning
+        if (mScanner != null) {
+            mScanner.stop();
+            mScanner = null;
+        }
+    }
+    // endregion
 }
