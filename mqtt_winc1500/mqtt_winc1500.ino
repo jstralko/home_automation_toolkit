794d3148252e1c55ff826db840a832abe24f6570
Gerald J Stralko <gerald.stralko@wolterskluwer.com>
2017-04-29 18:02:40 -0400

    add comands to work with google home

--- mqtt_winc1500/mqtt_winc1500.ino
+++ mqtt_winc1500/mqtt_winc1500.ino
@@ -25,6 +25,26 @@ int keyIndex = 0;                // your network key Index number (needed only f
 
 int status = WL_IDLE_STATUS;
 
+/*** NEOPixel setup ****/
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+// Which pin on the Arduino is connected to the NeoPixels?
+// On a Trinket or Gemma we suggest changing this to 1
+#define PIN            10
+
+// How many NeoPixels are attached to the Arduino?
+#define NUMPIXELS      15
+
+// When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
+// Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
+// example for more information on possible values.
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+int delayval = 500; // delay for half a second
+
 /************************* Adafruit.io Setup *********************************/
 
 #define AIO_SERVER      "io.adafruit.com"
@@ -56,6 +76,14 @@ Adafruit_MQTT_Subscribe sub = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feed
 #define LEDPIN 13
 
 void setup() {
+  // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
+#if defined (__AVR_ATtiny85__)
+  if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
+#endif
+  // End of trinket special code
+
+  pixels.begin(); // This initializes the NeoPixel library.
+  
   WiFi.setPins(WINC_CS, WINC_IRQ, WINC_RST, WINC_EN);
 
   while (!Serial);
@@ -92,21 +120,20 @@ void loop() {
       Serial.print(("Got: "));
       Serial.println((char *)sub.lastread);
 
-      if (0 == strcmp((char *)sub.lastread, "OFF")) {
-        digitalWrite(LEDPIN, LOW);
-      }
-      if (0 == strcmp((char *)sub.lastread, "ON")) {
-        digitalWrite(LEDPIN, HIGH);
+      if (0 == strcmp((char *)sub.lastread, "lights")) {
+        runLights();
+      } else if (0 == strcmp((char *)sub.lastread, "lightsoff")) {
+        turnOffLights();
       }
     }
   }
 
   // Now we can publish stuff!
-  if (! pub.publish(x++)) {
-    Serial.println(("Failed"));
-  } else {
-    Serial.println(("OK!"));
-  }
+  //if (! pub.publish(x++)) {
+  //  Serial.println(("Failed"));
+  //} else {
+  //  Serial.println(("OK!"));
+  //}
 
 }
 
@@ -145,3 +172,32 @@ void MQTT_connect() {
   }
   Serial.println("MQTT Connected!");
 }
+
+void runLights() {
+  
+  for(int i=0;i<NUMPIXELS;i++){
+
+    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+    pixels.setPixelColor(i, pixels.Color(0,150,0)); // Moderately bright green color.
+
+    pixels.show(); // This sends the updated pixel color to the hardware.
+
+    delay(delayval); // Delay for a period of time (in milliseconds).
+
+  }
+}
+
+void turnOffLights() {
+  
+  for(int i=0;i<NUMPIXELS;i++){
+
+    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+    pixels.setPixelColor(i, pixels.Color(0,0,0)); // Moderately bright green color.
+
+    pixels.show(); // This sends the updated pixel color to the hardware.
+
+    delay(delayval); // Delay for a period of time (in milliseconds).
+
+  }
+}
+
