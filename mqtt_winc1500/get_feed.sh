2e23d551a84cd61ca56f3f25e041b70efb3305c9
Gerald J Stralko <gerald.stralko@wolterskluwer.com>
2017-04-29 15:24:44 -0400

    add curl script to get and post data to my feeds

---
+++ mqtt_winc1500/get_feed.sh
@@ -0,0 +1,1 @@
+curl -H 'X-AIO-Key:8039edb08a0fe574097a021c0d0b4d9f8ba48f41' -v https://io.adafruit.com/api/feeds/hello.json
---
+++ mqtt_winc1500/post_new_data.sh
@@ -0,0 +1,1 @@
+curl -X POST -d '{ value:36 }' -H 'Content-Type: application/json' -H 'X-AIO-Key:8039edb08a0fe574097a021c0d0b4d9f8ba48f41' -v https://io.adafruit.com/api/feeds/hello/data.json
